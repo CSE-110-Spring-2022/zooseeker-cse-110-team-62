fdedce2ead7bca097f146664891b89761b1027ca
Jaden Padua <jadenpadua@Jadens-MBP.lan>
2022-04-29 16:52:12 -0700

    add comment

--- app/src/main/java/com/example/zooseeker_t62/MainActivity.java
+++ app/src/main/java/com/example/zooseeker_t62/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // search activity
         Intent intent = new Intent(this, SearchPage.class);
         startActivity(intent);
     }
